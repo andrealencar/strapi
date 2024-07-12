51ce266cb0547714b0710293ebac350af68571d2
mac <andre@aalencar.com.br>
2024-07-12 19:53:24 -0300

    initial

--- config/database.js
+++ config/database.js
@@ -1,9 +1,21 @@
+const {join} = require("node:path");
+
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-        client: 'postgres',
-        connection: {
-            connectionString: env('DATABASE_PRIVATE_URL')
-        },
-        pool: { min: 0 }
-    }
+      filename: join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
+  },
 });
+
+// module.exports = ({ env }) => ({
+//     connection: {
+//         client: 'postgres',
+//         connection: {
+//             connectionString: env('DATABASE_PRIVATE_URL')
+//         },
+//         pool: { min: 0 }
+//     }
+// });
